a4b7b93643a6726f078d889942bf69ae06c2d07b
paula-iskander <120270019+paula-iskander@users.noreply.github.com>
2023-10-07 08:33:02 +0300

    Patient and Pharmacist Models uploaded (Paula)

---
+++ src/Models/Patient.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GenderEnum = {
+    MALE: 'Male',
+    FEMALE: 'Female',
+  };
+
+const PatientSchema = new Schema({
+    Username: {
+      type: String,
+      required: true,
+    },
+    Name: {
+      type: String,
+      required: true
+    },
+    Email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    Password: {
+        type: String,
+        required: true
+    },
+    Gender: {
+        type: GenderEnum,
+        required: true
+    },
+    HourlyRate: {
+        type: Number,
+        required: true 
+    },
+    Affiliation: {
+        type: String,
+        required: true
+    },
+    EducationalBackground: {
+        type: String,
+        required: true 
+    }
+  }, { timestamps: true });
+  
+  const Patient = mongoose.model('Patient', PatientSchema);
+  module.exports = Patient;
---
+++ src/Models/Pharmacist.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PharmacistSchema = new Schema({
+    Username: {
+      type: String,
+      required: true,
+    },
+    Name: {
+      type: String,
+      required: true
+    },
+    Email: {
+      type: String,
+      required: true, 
+      unique:true
+    },
+    Password: {
+        type: String,
+        required: true
+    },
+    DateOfBirth: {
+        type: Date,
+        required: true
+    },
+    HourlyRate: {
+        type: Number,
+        required: true 
+    },
+    Affiliation: {
+        type: String,
+        required: true
+    },
+    EducationalBackground: {
+        type: String,
+        required: true 
+    }
+  }, { timestamps: true });
+  
+  const Pharmacist = mongoose.model('Pharmacist', PharmacistSchema);
+  module.exports = Pharmacist;
