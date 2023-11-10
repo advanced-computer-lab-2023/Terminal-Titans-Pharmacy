b102d2cd20b638249f9eee4182388e7d8bce7a3f
Seif Hossam <seif.hossam26092002@gamil.com>
2023-11-10 23:49:04 +0200

    Refactor order schema in Orders.js

--- src/Models/Orders.js
+++ src/Models/Orders.js
@@ -1,27 +1,30 @@
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
-    items: [
-      {
-        medicineId: { type: mongoose.Schema.Types.ObjectId, ref: 'Medicine' },
-        quantity: Number,
-        price: Number,
-      },
-    ],
-    total: Number,
-    status: {
-      type : String,
-      required: false
+  items: [
+    {
+      medicineId: { type: mongoose.Schema.Types.ObjectId, ref: 'Medicine' },
+      quantity: Number,
+      price: Number,
     },
-    address: {
-      type : String,
-      required: false
-    }
-  });
+  ],
+  total: Number,
+  status: {
+    type: String,
+    required: false
+  },
+  address: {
+    type: String,
+    required: false
+  },
+  paymentMethod: {
+    type: String,
+    required: true
+  },
+});
 
-  
-  const Order = mongoose.model('Order', orderSchema);
-  module.exports = Order;
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
